7463624e01fb26f624bb578a29ba149b63eb7058
parikshit <parikshitabuj94@gmail.com>
2026-01-27 19:52:05 +0530

    init commit

---
+++ src/Nestedforloop.java
@@ -0,0 +1,12 @@
+//parikshitabuj
+public class Nestedforloop {
+    public static void main(String[] args) {
+        for (int i=1; i<=3 ; i++){
+            for( int j=1; j<=10; i++){
+                System.out.println(i + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
