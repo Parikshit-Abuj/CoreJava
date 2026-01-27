acf6090f492b000d9c6ef7d53dae1beabbf06d6a
parikshit <parikshitabuj94@gmail.com>
2026-01-27 18:34:51 +0530

    init commit

---
+++ src/HappynewyearForloop.java
@@ -0,0 +1,21 @@
+//parikshitabuj
+import java.util.Scanner;
+public class HappynewyearForloop {
+    public static void main(String[] args) throws InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("how many seconds to coundownfrom: ");
+
+        int start = scanner.nextInt();
+
+        for (int i=start; i>0; i--){
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+
+        System.out.println("happy new year");
+
+
+    }
+}
