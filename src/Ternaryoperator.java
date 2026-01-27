d532d5578f40a938594a6ce441324998d5687838
parikshit <parikshitabuj94@gmail.com>
2026-01-27 12:12:37 +0530

    init commit

---
+++ src/Ternaryoperator.java
@@ -0,0 +1,9 @@
+//parikshitabuj
+public class Ternaryoperator {
+    public static void main(String[] args) {
+        int income = 60000;
+        double taxrate = (income >= 40000) ? 0.25 : 0.45;
+        System.out.println(taxrate);
+
+    }
+}
