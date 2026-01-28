52589adfff294148a7e650f1efeff6e71b4f525e
parikshit <parikshitabuj94@gmail.com>
2026-01-28 09:45:56 +0530

    init commit

---
+++ src/Variablescope.java
@@ -0,0 +1,12 @@
+//parikshitabuj
+public class Variablescope {
+        int x=3; // class
+    public static void main(String[] args) {
+        int x= 1;// Local variable
+    }
+
+    static void doSometihng(String[] args) {
+        int x = 4;//Local variable
+
+    }
+}
