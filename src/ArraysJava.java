8acf63744bc356c369c786ab22ccaac56ee959b4
parikshit <parikshitabuj94@gmail.com>
2026-01-28 12:25:25 +0530

    init commit

---
+++ src/ArraysJava.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class ArraysJava {
+    public static void main(String[] args) {
+        String[] fruits = {"apple,banana,orange,mango"};
+        fruits[0]= "pineapple";
+        int numOfFruits = fruits.length;
+        Arrays.sort(fruits);
+        Arrays.fill(fruits, "pineapple");
+        for(String fruit : fruits){
+            System.out.println(fruit);
+
+        String[] foods = new String[3];
+        foods[0] = "pizza";
+        foods[1] = "taco";
+        foods[2] = "burger";
+            for(String food: foods){
+                System.out.println(food);
+            }
+        }
+
+    }
+}
