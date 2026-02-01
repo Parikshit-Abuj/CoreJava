1adae2b37df7c7c565fc3147e1ac778aa320aa66
Parikshit <parikshitabuj94@gmail.com>
2026-02-01 12:33:33 +0530

    init commit

---
+++ JavaOops/StaticKeyword/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Friend f1 = new Friend("parikshit");
+        Friend f2 = new Friend("allen");
+        Friend f3 = new Friend("karan");
+        Friend f4 = new Friend("karen");
+
+        Friend.showFriends();
+
+    }
+}
+//static makes variable belong to the class
